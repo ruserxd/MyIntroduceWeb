1be6983692517dc7ba3db2e836dd9a8990a2a5df
ruserxd <zxcmomo0219@gmail.com>
2024-06-04 11:42:49 +0800

    import particles for top and new CSS for that behind component

---
+++ JavaScript/JS.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    particlesJS.load('particles-js', 'JsonFile/particlesjs-config.json', function() {
+        console.log('particles.js config loaded');
+    });
+});
